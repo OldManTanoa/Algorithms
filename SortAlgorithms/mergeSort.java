31afcc7a378372b79e536fca2f6db51609e791f4
OldManTanoa <zogg.ryan@gmail.com>
2022-12-24 10:28:38 +0100

    MergeSort cont

--- SortAlgorithms/mergeSort.java
+++ SortAlgorithms/mergeSort.java
@@ -10,5 +10,23 @@ public class mergeSort {
         int left = 0;
         int right = A.length;
         int middle = (int) Math.floor((left+right)/2);
+
+        mergeSort(A, left, middle);
+        mergeSort(A, middle, right);
+        merge(A, left, middle, right);
+    }
+
+    public static void mergeSort(int[] A, int left, int right) {
+        if(!(left == right)) {
+            int middle = (int) Math.floor((left+right)/2);
+
+            mergeSort(A, left, middle);
+            mergeSort(A, middle, right);
+            merge(A, left, middle, right);
+        }
+    }
+
+    public static void merge(int[] A, int left1, int middle, int right2) {
+
     }
 }
