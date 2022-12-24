dac4f12ac1184e30e62b301ae668a41ce9eefb31
OldManTanoa <zogg.ryan@gmail.com>
2022-12-24 10:35:21 +0100

    Finish mergeSort

--- SortAlgorithms/mergeSort.java
+++ SortAlgorithms/mergeSort.java
@@ -17,7 +17,7 @@ public class mergeSort {
     }
 
     public static void mergeSort(int[] A, int left, int right) {
-        if(!(left == right)) {
+        if(left < right) {
             int middle = (int) Math.floor((left+right)/2);
 
             mergeSort(A, left, middle);
@@ -26,7 +26,32 @@ public class mergeSort {
         }
     }
 
-    public static void merge(int[] A, int left1, int middle, int right2) {
+    public static void merge(int[] A, int left, int middle, int right) {
+        int i = left;
+        int j = middle;
+        int[] res = new int[right-left];
+        int k = 0;
 
+        while(i < middle && j < right) {
+            if (A[i] < A[j]) {
+                res[k] = A[i];
+                i++; k++;
+            } else {
+                res[k] = A[j];
+                j++; k++;
+            }
+        }
+
+        while (i < middle) {
+            res[k] = A[i];
+            k++;
+            i++;
+        }
+
+        while (j < right) {
+            res[k] = A[j];
+            j++;
+            k++;
+        }
     }
 }
