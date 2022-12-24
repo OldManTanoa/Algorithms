73a77ea1e43be8285d9016750288331c96aff041
OldManTanoa <zogg.ryan@gmail.com>
2022-12-24 10:23:05 +0100

    MergeSort start

--- SortAlgorithms/mergeSort.java
+++ SortAlgorithms/mergeSort.java
@@ -1,1 +1,14 @@
+package SortAlgorithms;
 
+public class mergeSort {
+    
+    public static void main(String[] args) {
+
+    }
+
+    public static void mergeSort(int[] A) {
+        int left = 0;
+        int right = A.length;
+        int middle = (int) Math.floor((left+right)/2);
+    }
+}
